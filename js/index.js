bc502f58f1fd0fb7f56daf2c83d3e66fc64129d9
Sean Fox <geekypghrunner@gmail.com>
2018-06-11 09:59:48 -0400

    fix: Service worker registers

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-navigator.serviceWorker.register('sw.js').then(reg => {
+navigator.serviceWorker.register('/sw.js').then(reg => {
     console.log('Service worker registered:', reg);
 }).catch(function(err) {
     console.log('Service worker reg fail:', err);
